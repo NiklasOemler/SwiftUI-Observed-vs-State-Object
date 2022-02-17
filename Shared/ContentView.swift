17e78ca9707026e22bf669418b0740db4536cb55
Niklas Oemler <niklas.oemler@gal-digital.de>
2022-02-17 14:36:59 +0100

    :tada: show difference betweent stateObject and ObserveredObject Wrapper

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+class StateObjectClass: ObservableObject {
+    enum ObserverType: String {
+        case stateObject
+        case observedObject
+    }
+    
+    @Published
+    var count = 0
+    
+    let type: ObserverType
+    let id = UUID()
+    
+    init(type: ObserverType) {
+        self.type = type
+    }
+    
+    deinit {
+        print(#function, "type: \(type.rawValue) id: \(id)")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct StateObjectCounter: View {
+    @StateObject
+    var state = StateObjectClass(type: .stateObject)
+    
+    var body: some View {
+        VStack {
+            Text("@StateObject's count: \(state.count)")
+            Button("Add 1"){
+                state.count += 1
+            }
+        }
+    }
+}
+
+struct ObservedObjectCounter: View {
+    @ObservedObject
+    var state = StateObjectClass(type: .observedObject)
+    
+    var body: some View {
+        VStack {
+            Text("@ObservedObject's count: \(state.count)")
+            Button("Add 1") {
+                state.count += 1
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State
+    private var count = 0
+    
+    var body: some View {
+        VStack {
+            Text("Refresh CounterView's count: \(count)")
+            Button("Refresh") {
+                count += 1
+            }
+            StateObjectCounter()
+                .padding()
+            ObservedObjectCounter()
+                .padding()
+        }
     }
 }
