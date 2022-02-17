f24076b4aecb3fd40dabf622447e426470377c96
Niklas Oemler <niklas.oemler@gal-digital.de>
2022-02-17 14:27:58 +0100

    Initial Commit

---
+++ Shared/ObservedVsStateObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedVsStateObjectApp.swift
+//  Shared
+//
+//  Created by Niklas Oemler on 17.02.22.
+//
+
+import SwiftUI
+
+@main
+struct ObservedVsStateObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
